2f56ca77e2a07c9df2b9f5b27bfaf8ec13e7617c
Krystan HuffMenne <krystan@tilde.io>
2023-01-11 10:16:59 -0800

    --wip-- [skip ci]

--- transforms/helpers/is-record.ts
+++ transforms/helpers/is-record.ts
@@ -1,4 +1,4 @@
 /** Checks if the given value is a `Record<string, unknown>`. */
-export function isRecord(value: unknown): value is Record<string, unknown> {
+export function isRecord<R extends Record<string, unknown>>(value: unknown): value is R {
   return value !== null && typeof value === 'object';
 }
